17c4df8da0b7c164fbb7fee3d98d03b102943290
Anna8742 P <anna.papka014@gmail.com>
2024-02-01 20:25:16 +0000

    1

---
+++ javascript.js
@@ -0,0 +1,33 @@
+/*
+Comments
+*/
+
+/* Declaring variable */
+var a;
+
+/* Assigning variable */
+var b = 2;
+
+var myNumber = 11;
+
+myNumber--;
+myNumber++;
+
+var z = 3;
+a += 12;
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\" ";
+console.log(myStr);
+/***
+ *      \' single quote
+ *      \" double quote
+ *      \\ backslash
+ *      \n newline
+ *      \r carriage return
+ *      \t tab
+ *      \b backspace
+ *      \f form feed
+ * 
+ * 
+ * 
+ */
